a773442867951c76f378d0e93b5198ddf784a367
iaei <649088838@qq.com>
2017-06-11 19:47:38 +0800

    基本完成

--- zhihu/js/index.js
+++ zhihu/js/index.js
@@ -1,7 +1,25 @@
-var $signIn = document.getElementById('changeToSignup');
-$signIn.addEventListener("click" ,function(){
-	this.setAttribute("class", "active")
+var signUpbutton = document.getElementById('changeToSignup');
+var signUp = document.getElementById("signup");
+var signInbutton = document.getElementById("changeToSignin");
+var signIn= document.getElementById("signin");
+var line = document.getElementById("chooseLine");
 
-})
+console.log(signUpbutton);
+console.log(changeToSignin);
+signInbutton.onclick = function () {
+	signUpbutton.setAttribute("class","null");
+	this.setAttribute("class","active");
+	signUp.style.cssText ="display:none";
+	signIn.style.cssText = "display:block";
+	line.style.transform = "translateX(72px)";
 
-	
+
+}
+
+signUpbutton.onclick = function () {
+	signInbutton.setAttribute("class","null");
+	this.setAttribute("class","active");
+	signIn.style.cssText ="display:none";
+	signUp.style.cssText = "display:block";
+	line.style.transform = "translateX(0px)"
+}
